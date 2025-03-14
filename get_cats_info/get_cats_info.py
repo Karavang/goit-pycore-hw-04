7d88c9d16714f76f74f46a37fb683f56200bff28
Karavang <k4r4v4ng@gmail.com>
2025-03-15 00:29:15 +0100

    get_cats_info

--- get_cats_info/get_cats_info.py
+++ get_cats_info/get_cats_info.py
@@ -1,2 +1,16 @@
-def get_cats_info():
-    
+def get_cats_info(path):
+    try:
+        with open(path) as file:
+            data = file.readlines()
+            infoCats = []
+            for cat in data:
+                objCat = cat.strip().split(",")
+
+                infoCats.append({"id": objCat[0], "name": objCat[1], "age": objCat[2]})
+            print(infoCats)
+
+    except:
+        return "Щось не так з файлом"
+
+
+print(get_cats_info("./get_cats_info/cats.txt"))
