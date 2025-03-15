9f9c70d4f6c9b0d3b1488b184f1ed7a44ae56660
Karavang <k4r4v4ng@gmail.com>
2025-03-16 00:11:57 +0100

    shitcursion

--- commander/fread.py
+++ commander/fread.py
@@ -2,21 +2,27 @@ from pathlib import Path
 from colorama import Fore
 from sys import argv
 
-
-def fread():
-    try:
-        route = Path(argv[1])
-
-        for path in route.iterdir():
-
-            if path.suffix:
-                print(Fore.BLUE, path)
-
-            else:
-                print(Fore.YELLOW, path)
-
-    except:
-        print("Не корректно вказаний роут")
+startRoute = Path(argv[1])
 
 
-fread()
+def fread(route):
+    global startRoute
+    # try:
+    Path(route)
+    contain = []
+    for path in Path(route).iterdir():
+
+        if path.suffix:
+            print(Fore.BLUE, path)
+            contain.append(str(path))
+
+        else:
+            print(Fore.YELLOW, path)
+            startRoute = str(startRoute) + "/" + str(path)
+            contain.append(fread(startRoute))
+        print(contain)
+    # except:
+    #     print("Не корректно вказаний роут")
+
+
+fread(startRoute)
